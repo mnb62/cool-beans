d923897446f0d85cda3f3a2baab53c9c474f1c37
frzrbox <frzrbox@frzrboxs-MacBook-Pro.local>
2021-02-15 01:06:47 -1000

    feat: cursor changes based off the data-cursor values of certain elements

---
+++ finished/scripts.js
@@ -0,0 +1,39 @@
+// Track mouse position and add custom properties to the body
+window.addEventListener('mousemove', (e) => {
+	document.body.style = `--x: ${e.clientX}px; --y: ${e.clientY}px;`;
+});
+
+// Create functions to easily change the cursor and reset it
+const cursorEl = document.querySelector('.cursor');
+
+function changeCursor(value) {
+	cursorEl.setAttribute('data-type', value);
+}
+
+function resetCursor() {
+	cursorEl.setAttribute('data-type', 'default');
+}
+
+// When an item contains the data-cursor attribute, change the cursor the the value
+const cursorItems = document.querySelectorAll('[data-cursor]');
+
+cursorItems.forEach((item) => {
+	const cursorValue = item.getAttribute('data-cursor');
+
+	item.addEventListener('mouseover', () => changeCursor(cursorValue));
+
+	item.addEventListener('mouseleave', () => resetCursor());
+});
+
+// Autoplay videos on hover
+const projectVideos = document.querySelectorAll('.project-item video');
+
+projectVideos.forEach((video) => {
+	video.addEventListener('mouseover', () => {
+		video.play();
+	});
+
+	video.addEventListener('mouseleave', () => {
+		video.pause();
+	});
+});
